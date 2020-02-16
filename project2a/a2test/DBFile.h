2636da7f10b5afde8a6a17a1d4c9faf910034817
Rohit K <krohit.bphc@gmail.com>
2020-02-16 12:30:13 -0500

    Project 1 dbfile code replaced

--- project2a/a2test/DBFile.h
+++ project2a/a2test/DBFile.h
@@ -8,25 +8,40 @@
 #include "Comparison.h"
 #include "ComparisonEngine.h"
 
-typedef enum {heap, sorted, tree} fType;
+typedef enum
+{
+	heap,
+	sorted,
+	tree
+} fType;
 
-// stub DBFile header..replace it with your own DBFile.h 
+// stub DBFile header..replace it with your own DBFile.h
 
-class DBFile {
+class DBFile
+{
+private:
+	File file;
+	char *filePath;
+	Record *current;
+	Page page;
+	off_t poff;  // int value of number of completed pages
+	off_t roff;  // reading page value
+	bool pDirty; // true on page changes else false
+	bool end;	// true on file end else false
 
 public:
-	DBFile (); 
+	DBFile();
+	~DBFile();
 
-	int Create (char *fpath, fType file_type, void *startup);
-	int Open (char *fpath);
-	int Close ();
+	int Create(char *fpath, fType file_type, void *startup);
+	int Open(char *fpath);
+	int Close();
 
-	void Load (Schema &myschema, char *loadpath);
+	void Load(Schema &myschema, char *loadpath);
 
-	void MoveFirst ();
-	void Add (Record &addme);
-	int GetNext (Record &fetchme);
-	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
-
+	void MoveFirst();
+	void Add(Record &addme);
+	int GetNext(Record &fetchme);
+	int GetNext(Record &fetchme, CNF &cnf, Record &literal);
 };
 #endif
